eb233de99bbb053452a2675783416e22d90bfbd8
Abu Haider Siddiq <dmsbilas@yahoo.com>
2025-03-15 13:01:19 +0600

    added new files

---
+++ second_program.py
@@ -0,0 +1,6 @@
+friends = ["A Rohim", "A Karim", "Jobbar", "Shofiq", "Sohag", "Sadik", "Mokarram", "Sumit", "Hridoy", "Priyom", "Deen", "Lalon"]
+
+for friend in friends:
+    print(friend, " Hello! ")
+
+
